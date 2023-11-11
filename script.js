8d16908f21500c3800983c09e958e5bd052ba11e
takumakadowaki <149216447+takumakadowaki@users.noreply.github.com>
2023-11-11 12:14:13 +0900

    更新

--- script.js
+++ script.js
@@ -1,29 +1,84 @@
-function addMemo() {
-    var input = document.getElementById('memoInput'); // メモ入力フィールドの要素を取得
-    var memo = input.value; // 入力された値を取得
-    if (memo) { // テキストが空でなければ
-      var li = document.createElement('li'); // 新しいリスト項目を作成
-      li.textContent = memo; // リスト項目のテキストにメモをセット
-  
-      var deleteBtn = document.createElement('button'); // 削除ボタンを作成
-      deleteBtn.textContent = 'Delete'; // ボタンのテキストをセット
-      deleteBtn.onclick = function() { // ボタンをクリックした時の動作を定義
-        li.remove(); // リスト項目を削除
-      };
-  
-      li.appendChild(deleteBtn); // リスト項目に削除ボタンを追加
-      document.getElementById('memoList').appendChild(li); // メモリストにリスト項目を追加
-  
-      input.value = ''; // 入力フィールドを空にする
-    }
-  }
-  
-  document.getElementById('addBtn').addEventListener('click', addMemo); // 'Add'ボタンにクリックイベントリスナーを追加
-  
-  // メモ入力フィールドでEnterキーが押された時にメモを追加するイベントリスナー
-  document.getElementById('memoInput').addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') {
-      addMemo();
-    }
-  });
+// デイトレードのメモを追加する関数
+function addTradeMemo() {
+  var dateInput = document.getElementById('dateInput').value;
+  var stockInput = document.getElementById('stockNameInput').value;  // 修正されたID
+  var purchasePriceInput = document.getElementById('purchasePriceInput').value;
+  var salePriceInput = document.getElementById('salePriceInput').value;
+  var memoInput = document.getElementById('memoInput').value;
 
+  if (dateInput && stockInput && purchasePriceInput && salePriceInput) {
+    var tradeMemo = {
+      date: dateInput,
+      stock: stockInput,
+      purchasePrice: purchasePriceInput,
+      salePrice: salePriceInput,
+      memo: memoInput
+    };
+    saveTradeMemoToStorage(tradeMemo);
+    displayTradeMemos();
+  }
+}
+
+// デイトレードのメモをローカルストレージに保存する関数
+function saveTradeMemoToStorage(tradeMemo) {
+  var tradeMemos = getTradeMemosFromStorage();
+  tradeMemos.push(tradeMemo);
+  localStorage.setItem('tradeMemos', JSON.stringify(tradeMemos));
+}
+
+// ローカルストレージからデイトレードのメモを取得する関数
+function getTradeMemosFromStorage() {
+  var tradeMemos = localStorage.getItem('tradeMemos');
+  if (tradeMemos) {
+    return JSON.parse(tradeMemos);
+  } else {
+    return [];
+  }
+}
+
+// デイトレードのメモを削除する関数
+function deleteTradeMemo(index) {
+  var tradeMemos = getTradeMemosFromStorage();
+  tradeMemos.splice(index, 1);
+  localStorage.setItem('tradeMemos', JSON.stringify(tradeMemos));
+  displayTradeMemos();
+}
+
+// デイトレードのメモを表示する関数
+function displayTradeMemos() {
+  var tradeMemos = getTradeMemosFromStorage();
+  var tradeMemoList = document.getElementById('tradeMemoList');
+  tradeMemoList.innerHTML = '';
+  tradeMemos.forEach((tradeMemo, index) => {
+    var li = document.createElement('li');
+    li.textContent = `${tradeMemo.date} - ${tradeMemo.stock} - Purchase: ${tradeMemo.purchasePrice} - Sale: ${tradeMemo.salePrice} - Memo: ${tradeMemo.memo}`;
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.onclick = function() {
+      deleteTradeMemo(index);
+    };
+
+    li.appendChild(deleteBtn);
+    tradeMemoList.appendChild(li);
+  });
+}
+
+// ページ読み込み時にデイトレードのメモを表示
+window.onload = function() {
+  displayTradeMemos();
+};
+
+// 証券番号と銘柄名のマッピング
+var stockMapping = {
+  '7203': 'トヨタ',
+  '6758': 'ソニー',
+  '9984': 'ソフトバンク'
+};
+
+// 証券番号入力フィールドにイベントリスナーを設定
+document.getElementById('stockNumberInput').addEventListener('input', function() {
+  var stockNumber = this.value;
+  var stockName = stockMapping[stockNumber];
+  document.getElementById('stockNameInput').value = stockName ? stockName : ''; // 修正されたID
+});
